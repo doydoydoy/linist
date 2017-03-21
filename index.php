06c4d8b578c25d17684e2d48da02f4841eff58a6
Jonathan Doydora <doydoydoy@users.noreply.github.com>
2017-03-21 17:02:08 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Linist - A Community for Comics</title>
+
+	<!-- Ubuntu Font -->
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+	<!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <!-- Font Awesome CDN -->
+  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></script>
+
+  <style type="text/css">
+
+  *, * *
+  {
+    box-sizing: border-box;
+  }
+
+  </style>
+
+</head>
+<body>
+<div style="background-color: #161616; padding: 5px 5px 0 5px;height: 10vh">
+<nav class="navbar navbar-default container" style="background-color: #161616; border: 0; margin-top: 0; margin-bottom: 0;">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#" style="color:white; font-family: 'Ubuntu'; font-weight: bold; letter-spacing: 3px;">
+        <span class="glyphicon glyphicon-pencil" style="font-size: 12px"></span> 
+        <span>linist</span>
+        </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <form class="navbar-form pull-left" style="padding: 0;">
+        <div class="input-group" >
+          <span class="input-group-addon" style="background-color: #f7f7f7" >
+            <span class="glyphicon glyphicon-search " ></span>
+          </span>
+          <input type="text" class="form-control" placeholder="Search Linist" style="background-color: #f7f7f7; width: 300px;">
+        </div>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#" style="color: white; font-weight: bold; padding: 15px 0; font-size: 15px;">Sign In</a></li>
+        <li><a style="color: #474747; font-weight: bold;  padding: 15px 0; font-size: 15px;">&nbsp;&nbsp;or&nbsp;&nbsp;</a></li>
+        <li><a href="#" style="color: white; font-weight: bold;  padding: 15px 0; font-size: 15px;">Sign Up</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
+
+<section style="background: linear-gradient(rgba(0, 0, 0, 0.15),rgba(0, 0, 0, 0.15)), url(banner.jpg) center/cover no-repeat; height: 90vh; position: relative;">
+  <div class="container">
+    <div class="row">
+      <div style="color:white; top: 50%; left: 50%; transform: translate(-50%, -50%); position: absolute; width: 20%;">
+        <h1 style="font-family: 'Ubuntu';" class="text-center">linist</h1>
+        <h3  class="text-center">Discover. Read. Draw.</h3>
+        <h5 class="text-center text-justify">Join and read new comics. Interact with the artists around here. Share your comics to the world.</h5>
+        <form>
+          <div style="padding: 10px 0">
+            <input type="button" value="Get Started" class="form-control btn btn-info"></input>
+          </div>
+          <div style="padding: 10px 0">
+            <input type="button" value="Sign In" class="form-control btn btn-default"></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  
+</section>
+
+</body>
+</html>
